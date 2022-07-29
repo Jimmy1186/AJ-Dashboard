57c83800638e0abe91d4a3835c83e4b74c441d81
Jimmy1186 <sususu089@gmail.com>
2022-07-29 11:22:34 +0800

    mui front v1

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -14,71 +14,78 @@ type navStateType = {
 
 function Nav({ navState, setNavState }: navStateType) {
   return (
-    <nav
-      className={` bg-black p-10 flex h-full flex-col items-center absolute z-10 transition-all  ${
-        navState ? "-translate-x-0 opacity-1" : " -translate-x-40 opacity-0"
-      } md:-translate-x-0 md:static md:opacity-100 md:p-2`}
-    >
-      {/* <nav
-      className={` bg-black p-10 flex h-full flex-col items-center absolute z-10 transition-all  ${
-        navState ? "-translate-x-0 opacity-1" : " -translate-x-40 opacity-0"
-      } md:-translate-x-0 md:static `}
-    ></nav> */}
-      <div className="logo text-white text-4xl bg-lime-500  flex justify-center items-center rounded-full w-12 h-12 mb-10">
-        <h1 className="">S</h1>
-      </div>
-      <ul className="list-none text-cyan-50 h-full rounded-md flex flex-col gap-10 text-3xl md:w-40">
-        <li className="flex flex-row items-center gap-3 md:text-2xl pl-2">
-          <Link href={"/"}>
-            <a
-              className="flex items-center  gap-3 "
-              onClick={() => setNavState(!navState)}
-            >
-              <HomeIcon />
-              首頁
-            </a>
-          </Link>
-        </li>
-        <li className="flex flex-row items-center gap-3 md:text-2xl pl-2">
-          <WorkIcon />
-          專案庫
-        </li>
-        <li className="flex flex-row items-center gap-3 md:text-2xl pl-2">
-          <AssignmentIcon />
-          任務
-        </li>
-        <li className="flex flex-row items-center gap-3 md:text-2xl pl-2">
-          <Link href={"/user_setting"}>
-            <a
-              className="flex items-center  gap-3 "
-              onClick={() => setNavState(!navState)}
-            >
-              <SettingsIcon />
-              設定
-            </a>
-          </Link>
-        </li>
-        <li className="flex flex-row items-center gap-3 md:text-2xl pl-2">
-          <Link href={"/admin"}>
-            <a
-              className="flex items-center  gap-3 "
-              onClick={() => setNavState(!navState)}
-            >
-              <AdminPanelSettingsIcon />
-              管理員
-            </a>
-          </Link>
-        </li>
-      </ul>
-      <Link href={"/user_setting"}>
-        <a
-          className="flex items-center  gap-3 "
-          onClick={() => setNavState(!navState)}
-        >
-          <Person />
-        </a>
-      </Link>
-    </nav>
+    <>
+      <nav
+        className={` bg-black p-10  flex h-full flex-col items-center absolute z-10 transition-all  ${
+          navState ? "-translate-x-0 opacity-1" : " -translate-x-40 opacity-0"
+        } md:-translate-x-0 md:static md:opacity-100 md:p-2`}
+      >
+        <div className="logo text-white text-4xl leading-normal bg-lime-500  flex justify-center items-center rounded-full w-12 h-12 mb-10">
+          <h1 className="">S</h1>
+        </div>
+        <ul className="list-none text-cyan-50 h-full rounded-md flex flex-col gap-10 text-3xl md:w-40">
+          <li className="flex flex-row items-center gap-3 md:text-2xl pl-2">
+            <Link href={"/"}>
+              <a
+                className="flex items-center  gap-3 "
+                onClick={() => setNavState(!navState)}
+              >
+                <HomeIcon />
+                首頁
+              </a>
+            </Link>
+          </li>
+          <li className="flex flex-row items-center gap-3 md:text-2xl pl-2">
+            <WorkIcon />
+            專案庫
+          </li>
+          <li className="flex flex-row items-center gap-3 md:text-2xl pl-2">
+            <AssignmentIcon />
+            任務
+          </li>
+          <li className="flex flex-row items-center gap-3 md:text-2xl pl-2">
+            <Link href={"/user_setting"}>
+              <a
+                className="flex items-center  gap-3 "
+                onClick={() => setNavState(!navState)}
+              >
+                <SettingsIcon />
+                設定
+              </a>
+            </Link>
+          </li>
+          <li className="flex flex-row items-center gap-3 md:text-2xl pl-2">
+            <Link href={"/admin"}>
+              <a
+                className="flex items-center  gap-3 "
+                onClick={() => setNavState(!navState)}
+              >
+                <AdminPanelSettingsIcon />
+                管理員
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <Link href={"/user_setting"}>
+          <a
+            className="flex items-center  gap-3 "
+            onClick={() => setNavState(!navState)}
+          >
+            <Person />
+          </a>
+        </Link>
+      </nav>
+
+      {/* 手機板讓按旁邊可以把nav消除 */}
+      {navState ? (
+        <div
+          className="h-screen absolute right-0 top-0 w-screen"
+          onClick={() => setNavState(false)}
+        ></div>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
 

3eba3dc4b0f63696efde37e699dfd89a34ba3f4d
Jimmy1186 <sususu089@gmail.com>
2022-08-04 15:05:06 +0800

    edit next auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,55 +1,34 @@
-import NextAuth from "next-auth"
-import { NextApiRequest, NextApiResponse } from 'next';
-import Credentials from "next-auth/providers/credentials";
+
 import { verify } from "argon2";
-import { PrismaClient } from '@prisma/client';
-// import { prisma } from "./prisma";
-// import { loginSchema } from "./validation/auth";
-
-
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-const options = {
-  secret: process.env.SECRET,
-  // When adapter is on - session doesn`t return user
-  // adapter: PrismaAdapter(prisma),
+
+import { NextApiHandler } from "next";
+import NextAuth, { NextAuthOptions } from "next-auth";
+
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const options: NextAuthOptions = {
   debug: true,
   providers: [
-    // EmailProvider({
-    //   server: {
-    //     host: process.env.EMAIL_SERVER_HOST,
-    //     port: process.env.EMAIL_SERVER_PORT,
-    //     auth: {
-    //       user: process.env.EMAIL_SERVER_USER,
-    //       pass: process.env.EMAIL_SERVER_PASSWORD
-    //     }
-    //   },
-    //   from: process.env.EMAIL_FROM
-    // }),
-    Credentials({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
+    CredentialsProvider({
+      id: "credentials",
+      name: "credentials",
+
       credentials: {
-        username: { label: 'UserName', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-        email:{label:"Email",type:'text'},
-
+        username: { label: "UserName", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
       },
-
       async authorize(credentials) {
         const data = {
           username: `${credentials?.username}`,
           password: `${credentials?.password}`,
-          email:`${credentials?.email}`,
         };
         // const user = await prisma.user.create( {data} );
         // return user
-   
+
         const registeredUser = await prisma.user.findFirst({
-          where: { username: { startsWith: credentials?.username } },
+          where: { username: { equals: credentials?.username } },
         });
         // We can throw errors if user exist or return user itself
         // Returning registeredUser
@@ -65,45 +44,131 @@ const options = {
         //   console.log(e);
         // }
         // login failed
-        
+
         return null;
       },
     }),
   ],
-  
+
+  secret: process.env.NEXTAUTH_SECRET,
+
+  jwt: {
+    maxAge: 60 * 60 * 24 * 30,
+  },
+  session: { strategy: "jwt", maxAge: 24 * 60 * 60, updateAge: 24 * 60 * 60 },
+
   callbacks: {
-    // Remove Any
-    jwt: async (params: any) => {
-      const { user, token } = params;
-      // saving userData into token manually
-      if (user) {
-        token.data = user;
+    async jwt({ token, account }) {
+      console.log("===== jwt =====");
+      console.log("token", token);
+      console.log("user", account);
+      
+      if (account) {
+        token.accessToken = account.access_token;
       }
       return token;
     },
-
-
-    // Remove Any
-    session: async (params: any) => {
-      const { token, session } = params;
-      if (session && token) {
-        session.id = token.id;
-        // saving userData from token to session
-        session.user = { ...token.data };
-        console.log(session)
-      }
+    async session({ session, token, user }) {
+      console.log("===== session =====");
+      console.log("token", token);
+      console.log("user", user);
+      session.accessToken = token.accessToken;
       return session;
     },
   },
-  pages: {
-    signIn: '/login',
-    signup: '/admin/signup',
-    error: '/auth/error', // Error code passed in query string as ?error=
-    verifyRequest: '/auth/verify-request', // (used for check email message)
-    newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-  }
-  
 };
 
-export default (req: NextApiRequest, res: NextApiResponse) =>
-  NextAuth(req, res, options);
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+export default authHandler;
+
+// const prisma = new PrismaClient();
+// const options = {
+//   secret: process.env.NEXTAUTH_SECRET,
+//   debug: true,
+//   providers: [
+//     Credentials({
+//       name: 'Credentials',
+//       credentials: {
+//         username: { label: 'UserName', type: 'text', placeholder: 'jsmith' },
+//         password: { label: 'Password', type: 'password' },
+//         email:{label:"Email",type:'text'},
+
+//       },
+
+//       async authorize(credentials) {
+//         const data = {
+//           username: `${credentials?.username}`,
+//           password: `${credentials?.password}`,
+//           email:`${credentials?.email}`,
+//         };
+//         // const user = await prisma.user.create( {data} );
+//         // return user
+
+//         const registeredUser = await prisma.user.findFirst({
+//           where: { username: { equals: credentials?.username } },
+//         });
+//         // We can throw errors if user exist or return user itself
+//         // Returning registeredUser
+//         if (registeredUser) return registeredUser;
+//         // try {
+//         //   // const hashed = await hash(data?.password, 10).then(
+//         //   //   (hasedPass) => (data.password = hasedPass)
+//         //   // );
+//         //   const user = await prisma.user.create( {data} );
+//         //   // New user successfully created
+//         //   return user;
+//         // } catch (e) {
+//         //   console.log(e);
+//         // }
+//         // login failed
+
+//         return null;
+//       },
+//     }),
+//   ],
+//   jwt: {
+//     // The maximum age of the NextAuth.js issued JWT in seconds.
+//     // Defaults to `session.maxAge`.
+//     maxAge: 60 * 60 * 24 * 30,
+//     // You can define your own encode/decode functions for signing and encryption
+//     async encode() {},
+//     async decode() {},
+//   },
+//   session: { strategy: "jwt",maxAge:24*60*60,updateAge: 24 * 60 * 60 },
+
+//   callbacks: {
+//     jwt: (token, user) => {
+//       console.log('===== jwt =====')
+//       console.log('token', token)
+//       console.log('user', user)
+//       if (user) {
+//         token = {
+//           accessToken: user.token,
+//           user: { username: user.username },
+//         }
+//       }
+//       return token
+//     },
+
+//     // Remove Any
+//     session: (session, token) => {
+//       console.log('===== session =====')
+//       console.log('session', session)
+//       console.log('token', token)
+//       return { user: token.user }
+//     },
+
+//   },
+
+//   pages: {
+//     signIn: '/login',
+//     signup: '/admin/signup',
+//     error: '/auth/error', // Error code passed in query string as ?error=
+//     verifyRequest: '/auth/verify-request', // (used for check email message)
+//     newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+//   }
+
+// };
+
+// export default (req: NextApiRequest, res: NextApiResponse) =>
+//   NextAuth(req, res, options);
